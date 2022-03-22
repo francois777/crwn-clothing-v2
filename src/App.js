8d1258fa842a78d124fcd598a65c5785379267ba
Francois van der Hoven <francoisvanderhoven@gmail.com>
2022-03-22 13:41:12 +1000

    Authentication refactored and completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './routes/home/home.component'
 import Navigation from './routes/navigation/navigation.component'
-import SignIn from './routes/sign-in/sign-in.component'
+import SignIn from './routes/authentication/authentication.component'
 
 const Shop = () => (
   <h1>I am the shop page</h1>
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -3,7 +3,8 @@ import {  getAuth,
           signInWithRedirect,
           signInWithPopup,
           GoogleAuthProvider,
-          createUserWithEmailAndPassword
+          createUserWithEmailAndPassword,
+          signInWithEmailAndPassword
 } from 'firebase/auth'
 import {
   getFirestore,
@@ -72,3 +73,9 @@ export const createAuthUserWithEmailAndPassword = async (email, password) => {
   if(!email || !password) return;
   return await createUserWithEmailAndPassword(auth, email, password)
 }
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await signInWithEmailAndPassword(auth, email, password);
+};
